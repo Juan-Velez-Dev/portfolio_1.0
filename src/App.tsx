b73e189b2305e528f060c80402f9798d982950dc
Juan Velez <juanvelez.dev@gmail.com>
2024-09-04 05:37:35 -0500

    flowbite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from "flowbite-react";
+import { NavBar } from "./NavBar";
+
 function App() {
   return (
     <>
-      <div className="bg-slate-400">
+      <NavBar />
+      <div className="bg-slate-400 w-16 h-36">
         <h1 className="bg-black">APP</h1>
+        <Button>Click me</Button>
       </div>
     </>
   );
